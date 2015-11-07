f06e3356ae66a55619556f208c18abc6812756ca
Eli Bronstein <ebronstein@berkeley.edu>
2015-11-07 12:14:09 -0800

    Add kilometers and miles option to vacationing_salesman.py

--- vacationing_salesman.py
+++ vacationing_salesman.py
@@ -1,25 +1,41 @@
 import sys
+import argparse
 from geopy.geocoders import Nominatim
 from geopy.distance import vincenty
 
-def get_distances(cities):
+def get_distances(cities, format):
 	locator = Nominatim()
 	geolocations = [locator.geocode(city) for city in cities]
 	distances = []
 	for i in range(len(cities)-1):
-		distances.append(vincenty(get_coordinates(geolocations[i]), get_coordinates(geolocations[i+1])).miles)
-	print_itinerary(cities, distances)
+		if format == "kilometers":
+			distances.append(vincenty(get_coordinates(geolocations[i]), get_coordinates(geolocations[i+1])).kilometers)
+		else:			
+			distances.append(vincenty(get_coordinates(geolocations[i]), get_coordinates(geolocations[i+1])).miles)
+	return distances
 
-def print_itinerary(cities, distances):
+def print_itinerary(cities, distances, format):
 	print("Success! Your vacation itinerary is:\n")
 	for i in range(len(distances)):
-		print("\t{0} -> {1}: {2} miles".format(cities[i], cities[i+1], round(distances[i], 2)))
-	print("\nTotal distance covered in your trip: {0} miles".format(round(sum(distances), 2)))
+		print("\t{0} -> {1}: {2} {3}".format(cities[i], cities[i+1], round(distances[i], 2), format))
+	print("\nTotal distance covered in your trip: {0} {1}".format(round(sum(distances), 2), format))
 
 def get_coordinates(geolocation):
 	return (geolocation.latitude, geolocation.longitude)
 
-cities = []
-for line in sys.stdin:
-	cities.append(line.strip())
-get_distances(cities)
+def make_itinerary(format):
+	cities = []
+	for line in sys.stdin:
+		cities.append(line.strip())
+	distances = get_distances(cities, format)
+	print_itinerary(cities, distances, format)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-m", "--miles", help="see the output in miles", action="store_true")
+	parser.add_argument("-k", "--kilometers", help="see the output in kilometers", action="store_true")
+	args = parser.parse_args()
+	if args.kilometers:
+	    make_itinerary("kilometers")
+	else:
+		make_itinerary("miles")
